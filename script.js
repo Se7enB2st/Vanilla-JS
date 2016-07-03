517c4a854dd0a0d83020a5965b55538c01213099
Se7enB2st <ethanhe1992@gmail.com>
2016-07-03 20:38:25 +0000

    Add for loop

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var todosList = {
   todos: [],
   displayTodos: function () {
-    console.log('My Todos:', this.todos);
+    if (this.todos.length === 0) {
+      console.log('Todolist is empty');
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+        if (this.todos[i].completed === true) {
+            console.log('(x)', this.todos[i].todoText);
+        } else {
+            console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
   },
+
   addTodo: function (todoText) {
     this.todos.push({
       todoText: todoText, //First todoText is name of property, 2nd one is the arguement
@@ -10,21 +23,26 @@ var todosList = {
     });
     this.displayTodos();
   },
+
   changeTodo: function (position, todoText) {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
+
   deleteTodo: function (position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
+
   toggleCompleted: function (position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
-  }
+  },
 };
 
-todosList.addTodo('Byeeee World');
-todosList.changeTodo(0, "Hello World");
+todosList.addTodo('Eat');
+todosList.addTodo('Sleep');
+todosList.addTodo('Code');
+
 todosList.toggleCompleted(0);
