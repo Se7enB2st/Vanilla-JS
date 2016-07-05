f5f9387a51f510db78bc6fefc1aea663628330e5
Se7enB2st <ethanhe1992@gmail.com>
2016-07-05 21:13:59 +0000

    Add toggleCompleted handler

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ var todosList = {
       for (var i = 0; i < this.todos.length; i++) {
         console.log(this.todos[i].todoText);
         if (this.todos[i].completed === true) {
-            console.log('(x)', this.todos[i].todoText);
+          console.log('(x)', this.todos[i].todoText);
         } else {
-            console.log('( )', this.todos[i].todoText);
+          console.log('( )', this.todos[i].todoText);
         }
       }
     }
@@ -41,7 +41,6 @@ var todosList = {
   },
 
   toggleAll: function () {
-    debugger;
     var totalTodos = this.todos.length;
     var completedTodos = 0;
     // Get number of completed todos
@@ -81,9 +80,6 @@ var handlers = {
   displayTodos: function () {
     todosList.displayTodos();
   },
-  toggleAll: function () {
-    todosList.toggleAll();
-  },
   addTodo: function () {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todosList.addTodo(addTodoTextInput.value);
@@ -98,7 +94,15 @@ var handlers = {
   },
   deleteTodo: function () {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todosList.deleteTodo(deleteTodoPositionInput.valueAsNumber)
+    todosList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = '';
+  },
+  toggleCompleted: function () {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todosList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+    toggleCompletedPositionInput.value='';
+  },
+  toggleAll: function () {
+    todosList.toggleAll();
   }
 };
